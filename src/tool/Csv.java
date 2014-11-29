801d36cbf5465aef707689b6f63153e70ee5107e
mimicman <red_stone_doing@yahoo.co.jp>
2014-11-29 15:29:33 +0900

    ファイル名定数化

--- src/tool/Csv.java
+++ src/tool/Csv.java
@@ -11,7 +11,7 @@ public class Csv {
 	static ArrayList<String[]> csvData = new ArrayList<String[]>();
 	
 	static ArrayList<String> Csvload(){
-		String filename = "./res/data/test.csv";
+		String filename = "sCSV_FILE_PATH";
 		
 		File file = new File(filename);
 		try {
