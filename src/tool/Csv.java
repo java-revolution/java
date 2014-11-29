386cae5f60547e64dc0cc838b28f9e78ea5375f8
mimicman <red_stone_doing@yahoo.co.jp>
2014-11-29 15:27:53 +0900

    test.csv内容変更

--- src/tool/Csv.java
+++ src/tool/Csv.java
@@ -7,15 +7,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Csv {
-	
-	/** プロジェクトルートからのパス */
-	private static final String sCSV_FILE_PATH = "./res/data/test.csv";
-	
 	static ArrayList<String> dir = new ArrayList<String>();
 	static ArrayList<String[]> csvData = new ArrayList<String[]>();
 	
 	static ArrayList<String> Csvload(){
-		String filename = sCSV_FILE_PATH;
+		String filename = "./res/data/test.csv";
 		
 		File file = new File(filename);
 		try {
