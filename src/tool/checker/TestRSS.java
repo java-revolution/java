c12d4244d5b1d918fb96719ee054a30c4844dc68
Takahiko-K <koseki.takahiko@gmail.com>
2014-12-21 23:16:52 +0900

    テスト用

---
+++ src/tool/checker/TestRSS.java
@@ -0,0 +1,15 @@
+package tool.checker;
+
+public class TestRSS {
+	public static void main(String[] sArgs) throws Exception {
+
+		for (int i = 0; true;) {
+
+			new RssChecker().Check("");
+			try {
+				Thread.sleep(60 * 1000 * 1); // 3000ミリ秒Sleepする
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
